033e714920e8ac78dafd991d514140cb300a5f66
Nkoskhona <nk0skh0n2@live.com>
2016-02-28 17:45:16 +0200

    class creation

---
+++ Fraction.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#ifndef Fraction_H
+#define	Fraction_H
+class Fraction
+{
+private:
+	int numarator;
+	int denominator;
+	int whole_num;
+
+public:
+
+	Fraction();
+	~Fraction();
+
+};
+
+
+#endif // !Fraction_H
+
+
+
